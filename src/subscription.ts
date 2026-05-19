13a2ec5f116a46cea6376741b82d766d861d5cb5
kunfei <gekunfei@live.com>
2026-05-19 22:13:23 +0800

    初始化

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,16 +1,28 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
 import { batchImportApps } from '@gkd-kit/tools';
 import categories from './categories';
-import globalGroups from './globalGroups';
+import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
+import { RawApp, RawAppGroup } from '@gkd-kit/api';
+
+const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+const rawApps: RawApp[] = [];
+apps.forEach((appConfig) => {
+  appConfig.groups?.forEach((g: RawAppGroup) => {
+    if (g.name.startsWith('开屏广告')) {
+      g.order = OPEN_AD_ORDER;
+    }
+  });
+  rawApps.push(appConfig);
+});
 
 export default defineGkdSubscription({
-  id: 320682112234,
-  name: 'Subscription',
+  id: 666,
+  name: 'AIsouler的GKD订阅-已停止维护',
   version: 0,
-  author: 'author',
-  checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  author: 'AIsouler',
+  checkUpdateUrl: './AIsouler_gkd.version.json5',
+  supportUri: 'https://github.com/AIsouler/GKD_subscription/issues/new/choose',
   categories,
   globalGroups,
-  apps: await batchImportApps(`${import.meta.dirname}/apps`),
+  apps: rawApps,
 });
