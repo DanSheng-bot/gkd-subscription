647473e08cffbe98f5783d8ba9fe869d17d8757c
kunfei <gekunfei@live.com>
2026-08-09 16:49:44 +0800

    build

--- src/apps/com.jdd.newjdd.app.ts
+++ src/apps/com.jdd.newjdd.app.ts
@@ -11,36 +11,9 @@ export default defineGkdApp({
       rules: [
         {
           fastQuery: true,
-          matches: '[vid="jump"][visibleToUser=true]',
-          exampleUrls: 'https://e.gkd.li/10dd2d8b-e48a-4554-a748-c9e156b003d9',
-          snapshotUrls: 'https://i.gkd.li/i/23294048',
-        },
-        {
-          fastQuery: true,
           activityIds: '.activity.SplashActivity',
-          matches: '[visibleToUser=true]',
-          matchDelay: 800,
-          action: 'back',
-        },
-      ],
-    },
-    {
-      key: 2,
-      name: '全屏广告',
-      rules: [
-        {
-          fastQuery: true,
-          activityIds: '.activity.MainActivity',
-          matches: '[vid="iv_close"][visibleToUser=true]',
-          exampleUrls: 'https://e.gkd.li/557eefdd-fd75-4a93-8f68-6ae8f042270a',
-          snapshotUrls: 'https://i.gkd.li/i/23294049',
-        },
-        {
-          fastQuery: true,
-          activityIds: '.activity.PopupPushActivity',
-          matches: '[id="android:id/content"][childCount=0]',
-          matchDelay: 300,
-          action: 'back',
+          matches: ['[text^="跳过"]'],
+          snapshotUrls: ['https://i.gkd.li/i/30869697'],
         },
       ],
     },
